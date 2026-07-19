e457aa677501c78c3afbf04a16d7194d94384ba8
Aarfdark <aadarsh.natarajan@gmail.com>
2026-07-19 00:40:29 -0400

    goad implemented

---
+++ MyFirstCharacterCode/Cards/Goad.cs
@@ -0,0 +1,32 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.DevConsole.ConsoleCommands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Afflictions;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MyFirstCharacter.MyFirstCharacterCode.Cards;
+
+namespace MyFirstCharacter.MyFirstCharacterCode.Cards;
+
+public class Goad() : MyFirstCharacterCard(-1,
+    CardType.Curse, CardRarity.Curse,
+    TargetType.None)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<VulnerablePower>(1)];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Unplayable];
+
+    public override int MaxUpgradeLevel => 0;
+    public override bool HasTurnEndInHandEffect => true;
+    
+    protected override async Task OnTurnEndInHand(PlayerChoiceContext choiceContext)
+    {
+        bool alreadyHasVuln = Owner.Creature.HasPower<VulnerablePower>();
+        PowerModel? powerModel = await PowerCmd.Apply<VulnerablePower>(choiceContext, Owner.Creature, DynamicVars.Vulnerable.BaseValue, null, this);
+        if (powerModel == null || alreadyHasVuln)
+            return;
+        powerModel.SkipNextDurationTick = true;
+    }
+}
